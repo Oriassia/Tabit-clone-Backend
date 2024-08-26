f7077d4dfac497375a9a86b34af9c705aade5286
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-08-26 10:14:38 +0300

    edit reservation, getReservation

--- src/routes/Reservation.route.ts
+++ src/routes/Reservation.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   addReservation,
   deleteReservation,
+  editReservation,
 } from "../controllers/Reservation.controller";
 
 export const reservationRoutes = Router();
@@ -9,3 +10,7 @@ export const reservationRoutes = Router();
 // Post
 reservationRoutes.patch("/add/:restaurantId", addReservation);
 reservationRoutes.delete("/:restaurantId/:reservationId", deleteReservation);
+reservationRoutes.patch(
+  "/:restaurantId/:oldTableId/:oldreservationId",
+  editReservation
+);
