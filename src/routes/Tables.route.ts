ae2bfd65f4b99924e2bf62f9d3d86935a3bd59fe
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-09-01 18:02:13 +0300

    elad

--- src/routes/Tables.route.ts
+++ src/routes/Tables.route.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { getAvailableTables } from "../controllers/Tables.controller";
+import {
+  getAvailableTables,
+  getTablePositionsForRest,
+} from "../controllers/Tables.controller";
 
 export const tablesRoute = Router();
 
 tablesRoute.post("/", getAvailableTables);
+tablesRoute.get("/position/:restId", getTablePositionsForRest);
