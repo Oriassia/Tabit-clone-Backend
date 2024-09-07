232bfd1562d0ca3ca40d57dfcfb441dbcd123126
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-09-08 01:09:43 +0300

    ori

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express from "express";
 import { restaurantsRoute } from "./routes/Restaurant.route";
 import cors from "cors";
 
@@ -8,32 +8,28 @@ import { giftCardRouter } from "./routes/Giftcards.route";
 
 const PORT = process.env.PORT || 3000;
 
-const app: Express = express();
+const app = express();
 
-async function main() {
-  // Connect to database
-  // await connectDB(); // Ensure the database connection is established
-  // Middleware
-  app.use(express.json());
-  app.use(
-    cors({
-      origin: ["https://tabit-clone.vercel.app"], //Frontend link
-      methods: ["POST", "GET", "UPDATE", "DELETE"],
-      credentials: true,
-    })
-  );
+// Connect to database
+// await connectDB(); // Ensure the database connection is established
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://tabit-clone.vercel.app"], //Frontend link
+    methods: ["POST", "GET", "UPDATE", "DELETE"],
+    credentials: true,
+  })
+);
 
-  // Routes
-  app.use("/api/tables", tablesRoute);
-  app.use("/api/restaurants", restaurantsRoute);
-  app.use("/api/reservations", reservationRoutes);
-  app.use("/api/giftcard", giftCardRouter);
+// Routes
+app.use("/api/tables", tablesRoute);
+app.use("/api/restaurants", restaurantsRoute);
+app.use("/api/reservations", reservationRoutes);
+app.use("/api/giftcard", giftCardRouter);
 
-  app.get("/", (req, res) => res.json("Express on Vercel"));
+app.get("/", (req, res) => res.json("Express on Vercel"));
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-main();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
